ac23341d495fe85aabe91a250a05468db1332e79
JoaoMarcosVilela <joaosemp12345@gmail.com>
2023-10-21 23:03:41 -0300

    Primeiro commit

---
+++ src/test/java/me/joao/apirestnuvem/ApiRestNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joao.apirestnuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
